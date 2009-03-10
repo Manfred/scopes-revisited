333803fa0c7e3b805edea07b063d8e40548446f6
Manfred Stienstra <manfred@fngtps.com>
2009-03-10 22:59:35 +0100

    I guess this is how it's implement now.

--- tmp/napkin.rb
+++ tmp/napkin.rb
@@ -1,11 +1,24 @@
 class Book
+  class << self
+    attr_accessor :scope_stack
+  end
+  @scope_stack = []
+  
   def self.with_scope(method, options={}, action=:merge, &block)
-    eval("@block_scope_options = #{options.inspect}", block.binding)
-    block.call
+    self.scope_stack << [method, options]
+    begin
+      block.call
+    ensure
+      self.scope_stack.pop
+    end
   end
   
   def self.find(action, options={})
-    p @block_scope_options
+    p scope_conditions(:find)
+  end
+  
+  def self.scope_conditions(method)
+    self.scope_stack.first
   end
 end
 
