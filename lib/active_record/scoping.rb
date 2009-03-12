42717342cedee9b6b8c880ef1fed89d8bd3caa70
Eloy Duran <eloy.de.enige@gmail.com>
2009-03-12 10:20:36 +0100

    Use extend instead of include to define the Scopes class methods.

--- lib/active_record/scoping.rb
+++ lib/active_record/scoping.rb
@@ -2,6 +2,6 @@ require 'active_record/scoping/scopes'
 
 module ActiveRecord
   class Base
-    include ActiveRecord::Scoping::Scopes
+    extend ActiveRecord::Scoping::Scopes
   end
 end
--- lib/active_record/scoping/scopes.rb
+++ lib/active_record/scoping/scopes.rb
@@ -3,27 +3,24 @@ require 'thread'
 module ActiveRecord
   module Scoping
     module Scopes
-      def self.included(base)
+      def self.extended(base)
         Thread.current[:scope_stacks] = {}
-        base.extend(ClassMethods)
       end
 
-      module ClassMethods
-        protected
-          def with_scope(method, scope_options, &block)
-            self.scope_stack << [method, scope_options]
-            begin
-              block.call
-            ensure
-              self.scope_stack.pop
-            end
+      protected
+        def with_scope(method, scope_options, &block)
+          self.scope_stack << [method, scope_options]
+          begin
+            block.call
+          ensure
+            self.scope_stack.pop
           end
+        end
 
-          def scope_stack
-            scope_stacks = Thread.current[:scope_stacks]
-            scope_stacks[name] ||= []
-          end
-      end
+        def scope_stack
+          scope_stacks = Thread.current[:scope_stacks]
+          scope_stacks[name] ||= []
+        end
     end
   end
 end
